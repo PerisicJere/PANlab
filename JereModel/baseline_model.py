4b70915f0a330417a221ff332adc65ffbb1f6ed3
Jerry <117933956+PerisicJere@users.noreply.github.com>
2024-04-07 17:15:16 -0400

    Add files via upload

--- JereModel/baseline_model.py
+++ JereModel/baseline_model.py
@@ -1,7 +1,8 @@
 from transformers import BertForSequenceClassification, BertTokenizer
+from sklearn.metrics import accuracy_score,f1_score
 import torch, tqdm
 import pandas as pd
-
+from prettytable import PrettyTable
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if device is True:
@@ -17,25 +18,30 @@ tokenizer = BertTokenizer.from_pretrained(model_name)
 model.to(device)
 
 base_df = pd.read_csv('trainEn.csv')
-label_map = {"CONSPIRACY": 0, "CRITICAL": 1}
+label2id = {"CONSPIRACY": 0, "CRITICAL": 1}
+
+myTable = PrettyTable(["Conspiracy", "Critical"])
 
 model.eval()
-correct_predictions = 0
-total_predictions = 0
+true_labels = []
+predicted_labels = []
 
 with torch.no_grad():
     for index,row in base_df.iterrows():
         text = row['Text']
         label = row['Label']
-        label_id = label_map[label]
+        label_id = label2id.get(label)
         inputs = tokenizer(text, return_tensors='pt', max_length=128, truncation=True, padding=True)
         inputs = {key: val.to(device) for key, val in inputs.items()}
         outputs = model(**inputs)
         logits = outputs.logits
-        _, predicted = torch.max(logits, 1)
-        total_predictions += 1
-        if predicted == label_id:
-            correct_predictions += 1
+        predicted = torch.softmax(logits, 1)
+        predicted_label_id = torch.argmax(predicted, axis=1).item()
+        true_labels.append(label_id)
+        predicted_labels.append(predicted_label_id)
 
-base_accuracy = correct_predictions / total_predictions
+f1_scores = f1_score(true_labels, predicted_labels, average=None)
+myTable.add_row(f1_scores)
+print(myTable)
+base_accuracy = accuracy_score(true_labels, predicted_labels)
 print(f"Baseline Accuracy: {base_accuracy}")
