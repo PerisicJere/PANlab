d60a306c53d4bfb0b6aa239ec7d31f1ae6b590e6
Jerry <117933956+PerisicJere@users.noreply.github.com>
2024-04-27 20:40:53 -0400

    Update Llama3_ner.py

--- Fine-tuned/Llama3_ner.py
+++ Fine-tuned/Llama3_ner.py
@@ -14,8 +14,7 @@ pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
     model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
-    token='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'
+    device_map="auto"
 )
 def get_ner(pipline):
     output_data = []
